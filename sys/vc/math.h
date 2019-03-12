03eaac249dbc3a97ac2c55b1d79457a66ac22116
Tuomo Venäläinen <tuomoster@gmail.com>
2019-03-12 12:18:05 +0200

    latest changes

--- sys/vc/math.h
+++ sys/vc/math.h
@@ -5,6 +5,7 @@
 
 #include <stdint.h>
 #include <endian.h>
+#include <mach/param.h>
 
 /*
  * IEEE 32-bit
@@ -27,15 +28,25 @@ union _ieee754f { uint32_t u32; float f; };
     (*(uint32_t *)&(f) = 0x7fffffffU)
 #define f_fsetsnan(f)                                                   \
     (*(uint32_t *)&(f) = 0xffffffffU)
-
+    
 /*
  * IEEE 64-bit
  * 0..51  - mantissa
  * 52..62 - exponent
  * 63     - sign
  */
-    union _ieee754d { uint64_t u64; double d; };
+#if (WORDSIZE == 8)
+union _ieee754d { uint64_t u64; double d; };
+#elif (LONGLONGSIZE == 8)
+#if !defined(UINT64_C)
+#define UINT64_C(u) u ## ULL
+#endif
+union _ieee754d { unsigned long long u64; double d };
+#elif (WORDSIZE == 4)
+union _ieee754d { uint32_t u32v[2]; double d; };
+#endif
 
+#if (WORDSIZE == 8) || (LONGLONGSIZE == 8)
 #define f_dgetmant(d)       (((union _ieee754d *)(&(d)))->u64 & UINT64_C(0x000fffffffffffff))
 #define f_dgetexp(d)        ((((union _ieee754d *)&(d))->u64 & UINT64_C(0x7ff0000000000000)) >> 52)
 #define f_dgetsign(d)       (((union _ieee754d *)(&(d)))->u64 & UINT64_C(0x8000000000000000))
@@ -56,17 +67,19 @@ union _ieee754f { uint32_t u32; float f; };
  * 64..78 - exponent
  * 79     - sign
  */
-#define f_ldgetmant(ld)       (*((uint64_t *)&(ld)))
-#define f_ldgetexp(ld)        (*((uint32_t *)&(ld) + 2) & 0x7fff)
-#define f_ldgetsign(ld)       (*((uint32_t *)&(ld) + 3) & 0x8000)
-#define f_ldsetmant(ld, mant) (*((uint64_t *)&(ld)) = (mant))
-#define f_ldsetexp(ld, exp)   (*((uint32_t *)&(ld) + 2) |= (exp) & 0x7fff)
-#define f_ldsetsign(ld, sign)                                           \
+union _ieee754ld80 { uint8_t u8[10]; long double d; };
+
+#define f_ldgetmant80(ld)       (*((uint64_t *)&(ld)))
+#define f_ldgetexp80(ld)        (*((uint32_t *)&(ld) + 2) & 0x7fff)
+#define f_ldgetsign80(ld)       (*((uint32_t *)&(ld) + 3) & 0x8000)
+#define f_ldsetmant80(ld, mant) (*((uint64_t *)&(ld)) = (mant))
+#define f_ldsetexp80(ld, exp)   (*((uint32_t *)&(ld) + 2) |= (exp) & 0x7fff)
+#define f_ldsetsign80(ld, sign)                                           \
     ((sign)                                                             \
      ? (*((uint32_t *)&ld + 3) |= 0x8000)                               \
      : (*((uint32_t *)&ld + 3) &= 0x7fff))
 #if (__BYTE_ORDER == __LITTLE_ENDIAN)
-#define f_ldsetnan(ld)                                                  \
+#define f_ldsetnan80(ld)                                                \
         do {                                                            \
             uint32_t *_u32p = (uint32_t *)&(ld);                        \
                                                                         \
@@ -74,7 +87,7 @@ union _ieee754f { uint32_t u32; float f; };
             _u32p[1] = 0xffffffffU;                                     \
             _u32p[2] = 0x7fffU;                                         \
         } while (0)
-#define f_ldsetsnan(ld)                                                 \
+#define f_ldsetsnan80(ld)                                               \
     do {                                                                \
         uint32_t *_u32p = (uint32_t *)&(ld);                            \
                                                                         \
@@ -83,7 +96,7 @@ union _ieee754f { uint32_t u32; float f; };
         _u32p[2] = 0xffffU;                                             \
     } while (0)
 #elif (__BYTE_ORDER == __LITTLE_ENDIAN)
-#define f_ldsetnan(ld)                                                  \
+#define f_ldsetnan80(ld)                                                \
     do {                                                                \
         uint32_t *_u32p = (uint32_t *)&(ld);                            \
                                                                         \
@@ -91,7 +104,79 @@ union _ieee754f { uint32_t u32; float f; };
         _u32p[1] = 0xffffffffU;                                         \
         _u32p[2] = 0xffffffffU;                                         \
     } while (0)
-#define f_ldsetsnan(ld)                                                 \
+#define f_ldsetsnan80(ld)                                               \
+    do {                                                                \
+        uint32_t *_u32p = (uint32_t *)&(ld);                            \
+                                                                        \
+        _u32p[0] = 0xffffU;                                             \
+        _u32p[1] = 0xffffffffU;                                         \
+        _u32p[2] = 0xffffffffU;                                         \
+    } while (0)
+#endif
+
+/*
+ * IEEE 128-bit
+ * 0..111       - mantissa
+ * 112..126     - exponent
+ * 127          - sign
+ */
+#if (WORDSIZE == 8)
+#define FD_SUBNORMAL_MASK       UINT64_C(0xffff000000000000)
+#define LD_EXP_MASK             UINT64_C(0xffff000000000000)
+#define LD_INFINITY_MASK        UINT64_C(0xffff000000000000)
+#define LD_NAN_MASK             { ~LD_EXP_MASK, UINT64_C(0xffffffffffffffff) }
+#define f_ldissub128(lp)        (!((lp)->u64v[0] & FD_SUBNORMAL_MASK))
+#define f_ld
+union _ieee754ld80 { uint8_t u64v[2]; long double d; };
+#elif (WORDSIZE == 4)
+union _ieee754ld80 { uint8_t u32v[4]; long double d; };
+#endif
+
+#define F_SUBNORMAL_EXP         0x0000
+#define F_INFINITY_EXP          0x7fff
+#define F_NAN_EXP               0x7fff
+
+#define f_ldgetmant128(ld)                                              \
+    (*((uint64_t *)&(ld)))
+#define f_ldgetexp128(ld)                                               \
+    (*((uint32_t *)&(ld) + 2) & 0x7fff)
+#define f_ldgetsign128(ld)                                              \
+    (*((uint32_t *)&(ld) + 3) & 0x12800)
+#define f_ldsetmant128(ld, mant)                                        \
+    (*((uint64_t *)&(ld)) = (mant))
+#define f_ldsetexp128(ld, exp)                                          \
+    (*((uint32_t *)&(ld) + 2) |= (exp) & 0x7fff)
+#define f_ldsetsign128(ld, sign)                                        \
+    ((sign)                                                             \
+     ? (*((uint32_t *)&ld + 3) |= 0x8000)                               \
+     : (*((uint32_t *)&ld + 3) &= 0x7fff))
+#if (__BYTE_ORDER == __LITTLE_ENDIAN)
+#define f_ldsetnan128(ld)                                               \
+    do {                                                                \
+        uint32_t *_u32p = (uint32_t *)&(ld);                            \
+                                                                        \
+        _u32p[0] = 0xffffffffU;                                         \
+        _u32p[1] = 0xffffffffU;                                         \
+        _u32p[2] = 0x7fffU;                                             \
+    } while (0)
+#define f_ldsetsnan128(ld)                                              \
+    do {                                                                \
+        uint32_t *_u32p = (uint32_t *)&(ld);                            \
+                                                                        \
+        _u32p[0] = 0xffffffffU;                                         \
+        _u32p[1] = 0xffffffffU;                                         \
+        _u32p[2] = 0xffffU;                                             \
+    } while (0)
+#elif (__BYTE_ORDER == __LITTLE_ENDIAN)
+#define f_ldsetnan128(ld)                                               \
+    do {                                                                \
+        uint32_t *_u32p = (uint32_t *)&(ld);                            \
+                                                                        \
+        _u32p[0] = 0x7fffU;                                             \
+        _u32p[1] = 0xffffffffU;                                         \
+        _u32p[2] = 0xffffffffU;                                         \
+    } while (0)
+#define f_ldsetsnan128(ld)                                              \
     do {                                                                \
         uint32_t *_u32p = (uint32_t *)&(ld);                            \
                                                                         \
