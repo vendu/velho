e59833823cbb0f36f5f0c6fb68c0c397858a846b
Tuomo Venäläinen <tuomoster@gmail.com>
2019-07-30 20:31:10 +0300

    added vcode files

---
+++ sys/vcode/conf.h
@@ -0,0 +1,12 @@
+#ifndef __V_CONF_H__
+#define __V_CONF_H__
+
+#define V_WORD_BITS     64 // # of bits in word
+#define V_POINTER_BITS  64 // # of bits in pointer
+#define V_ADDRESS_BITS  48 // # of significant bits in pointer
+#define V_LD_BITS       80 // # of bits in long double
+
+#define V_REGISTERS_MAX 64 // max # of registers
+
+#endif /* __V_CONF_H__ */
+
